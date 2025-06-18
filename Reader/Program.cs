c9debd7dee967f1218191e12dbe282f92184b5e2
hellokittyluvr73 <caramelmachiatto107@gmail.com>
2025-06-18 16:04:43 +0200

    files

---
+++ Reader/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Threading.Tasks;
+using BingoSyncAPI; // Core API
+using static BingoSyncAPI.BingoSync;
+using static BingoSyncAPI.BingoSyncTypes;
+public class Program
+{
+  public static async Task Main(string[] args)
+  {
+    BingoGame game = new BingoGame();
+    game.Init();
+    await game.JoinBingoSyncRoom();
+    string path = @"C://Bingo//match.txt";
+    string now = DateTime.Now.ToString("hh:mm:ss");
+    File.WriteAllText(path, now + Environment.NewLine);
+    Console.WriteLine("Press enter to close program");
+    Console.ReadLine();
+  }
+ 
+}
+public class BingoGame
+{
+  private readonly BingoSync bingoSync = new BingoSync();
+
+ public void Init()
+  {
+    bingoSync.OnMessageReceived += OnRoomEvent;
+  }
+
+
+  public async Task JoinBingoSyncRoom()
+  {
+
+    Console.WriteLine("Enter Room ID");
+    string ID = Console.ReadLine();
+    Console.WriteLine("Enter Room Password");
+    string password = Console.ReadLine();
+
+
+    var myRoomInfo = new RoomInfo
+    (
+        ID,   // Room ID
+        password,                    // Password
+        "The Watcher Knight",                     // Name
+        PlayerColors.Pink,          // Color
+        false                      // Spectator
+    );
+
+    if (await bingoSync.JoinRoom(myRoomInfo) == BingoSync.ConnectionStatus.Connected)
+    {
+      Console.WriteLine("Successfully connected to room!");
+
+      await bingoSync.SendChatMessage("Bingy!");
+    }
+    else
+    {
+      Console.WriteLine("Failed to connect.");
+    }
+
+    
+  }
+  private void OnRoomEvent(SocketMessage message)
+  {
+
+    string chatMessage = string.Empty;
+    int changedSquareOnBoard = 1;
+    string playerWhoMarked = string.Empty;
+    string goalMarked = string.Empty;
+    switch (message.type) // connection, goal, revealed, color, chat, new-card
+    {
+      case "chat":
+        chatMessage = message.text;
+        Console.WriteLine(chatMessage);
+        break;
+      case "goal":
+        if (int.TryParse(message.square.slot.Replace("slot", ""), out int result))
+          changedSquareOnBoard = result;
+        playerWhoMarked = message.player.name;
+        goalMarked = message.square.name;
+        string path = @"C://Bingo//match.txt";
+        string now = DateTime.Now.ToString("hh:mm:ss");
+        if (!File.Exists(path))
+        {
+          string createText = now + "," + playerWhoMarked + "," + goalMarked + "," + changedSquareOnBoard + Environment.NewLine;
+          File.WriteAllText(path, createText);
+        }
+        else
+        {
+          string createText = now + "," + playerWhoMarked + "," + goalMarked + "," + changedSquareOnBoard + Environment.NewLine;
+          File.AppendAllText(path, createText);
+        }
+        
+        //Console.WriteLine($"{now}, {playerWhoMarked}, {goalMarked}");
+
+        break;
+
+
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
---
+++ analyzer.py
@@ -0,0 +1,120 @@
+import pandas as pd
+from pathlib import Path
+import os
+dir = 'C://Bingo//statsAnalyzer'
+playerList = []
+row1 = range(1, 6)
+row2 = range(6, 11)
+row3 = range(11, 16)
+row4 = range(16, 21)
+row5 = range(21, 26)
+column1 = range(1, 23, 5)
+column2 = range(2, 24, 5)
+column3 = range(3, 25, 5)
+column4 = range(4, 26, 5)
+column5 = range(5, 27, 5)
+tlbr = range(1, 26, 6)
+bltr = range(5, 22, 4)
+#victory conditions
+
+
+overall = pd.DataFrame(columns = ["name", "FirstBlood", "GoalsMarked", "GoalsLost", "WinsByLine", "LossesByLine", "WinsByMajority", "LossesByMajority", "WinLoss", "Comeback", "Choke"])
+for file in Path(dir).rglob('*.txt'):
+    df = pd.read_csv(file, header=None)
+    df.columns = ['Time', 'Player', 'Goal', 'Position']
+    names = list(df['Player'].unique())
+    if len(names) == 1:
+            print(f"Match corresponding to file {file} only has one player marking goals. Please type the other player's name.") #TODO need to have var filename somewhere
+            otherplayer = input()
+            names.append(otherplayer) #if loser of match doesnt hit a single goal
+    
+
+
+    #now we also want to find who won the match
+    player1 = names[0]
+    player2 = names[1]
+    def score(x): #score for a given square
+        if x in df['Position'].values:
+            if df.loc[(df['Position'] == x), 'Player'].iloc[0] == player1:
+                   return(1)
+            if df.loc[(df['Position'] == x), 'Player'].iloc[0] == player2:
+                   return(-1)
+        else:
+             return(0)
+    row1 = range(1, 6)
+    row2 = range(6, 11)
+    row3 = range(11, 16)
+    row4 = range(16, 21)
+    row5 = range(21, 26)
+    column1 = range(1, 23, 5)
+    column2 = range(2, 24, 5)
+    column3 = range(3, 25, 5)
+    column4 = range(4, 26, 5)
+    column5 = range(5, 27, 5)
+    tlbr = range(1, 26, 6)
+    bltr = range(5, 22, 4)
+    def linescore(line):
+            value = 0
+            for x in line:
+                value = value + score(x)
+            return(int(value))
+    lines = [row1, row2, row3, row4, row5, column1, column2, column3, column4, column5, tlbr, bltr]
+    linescores = []
+    for any in lines:
+         linescores.append(linescore(any))
+    winner = []
+    majoritywinner = []
+    comeback = 0
+    choke = 0
+    goalsmarked = len(df['Position'])
+    if 5 in linescores:
+         winner = player1
+    if -5 in linescores:
+         winner = player2
+    if -5 not in linescores and 5 not in linescores:
+        majorityscore = 0
+        for x in range(1, 26):
+              majorityscore += score(x)
+        if majorityscore > 0:
+            majoritywinner = player1
+        if majorityscore < 0:
+            majoritywinner = player2
+
+    for name in names:
+        if name not in playerList:
+           playerList.append(name)
+        #contributes to playerList
+    firstBlood = df['Player'][0] #first to mark a goal
+    for name in names:
+        if name == otherplayer:
+            hits = 0    
+        else: 
+            hits = int(df['Player'].value_counts().get(name))
+        goalsLost = goalsmarked - hits
+        if winner == name or majoritywinner == name:
+             winloss = 1
+        else:
+             winloss = -1
+        if winloss == 1 & goalsLost > hits:
+             comeback = 1
+        else:
+             comeback = 0
+        if winloss == -1 & hits > goalsLost:
+             choke = 1
+        else:
+             choke = 0
+        newrow = pd.DataFrame([[name, int(firstBlood == name), hits, goalsLost, int(winner == name), int(winner != name), int(majoritywinner == name), int(majoritywinner != name), winloss, comeback, choke]], 
+                              columns=["name", "FirstBlood", "GoalsMarked", "GoalsLost", "WinsByLine", "LossesByLine", "WinsByMajority", "LossesByMajority", "WinLoss", "Comeback", "Choke"])
+        overall = pd.concat([overall, newrow], ignore_index=True) #one row per player per match
+              #end loop
+
+grouped = overall.groupby("name").sum()
+matchesplayed = []
+for user in playerList:
+     games = int(overall['name'].value_counts().get(user))
+     matchesplayed.append(games)
+
+grouped['Matches Played'] = matchesplayed
+grouped['GoalsPerGame'] = grouped['GoalsMarked'] / grouped['Matches Played']
+grouped['K/D'] = grouped['GoalsMarked'] / grouped['GoalsLost']
+print(grouped)
